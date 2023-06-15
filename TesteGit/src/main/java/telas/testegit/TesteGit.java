2191a87c8effeecd125f7719b7e68d016177ecc9
andre <andre@DESKTOP-LIUOUTR>
2023-06-15 11:44:56 -0300

    Merge origin/master
    Conflicts:
    	src/main/java/telas/Administrador.java
    	src/main/java/telas/TelaCadCursos.form
    	src/main/java/telas/TelaCadCursos.java
    	src/main/java/telas/TelaCadastro.form
    	src/main/java/telas/TelaCadastro.java
    	src/main/java/telas/TelaCursosDisp.form
    	src/main/java/telas/TelaCursosDisp.java
    	src/main/java/telas/TelaExtra.form
    	src/main/java/telas/TelaExtra.java
    	src/main/java/telas/TelaInicial.form
    	src/main/java/telas/TelaInicial.java
    	src/main/java/telas/TelaLogin.form
    	src/main/java/telas/TelaLogin.java
    	src/main/java/telas/TelaLoginAdm.form
    	src/main/java/telas/TelaLoginAdm.java
    	src/main/java/telas/TelaMenu.form
    	src/main/java/telas/TelaMenu.java
    	src/main/java/telas/TelaMenuAdm.form
    	src/main/java/telas/TelaMenuAdm.java
    	src/main/java/telas/TelaMeusCursos.form
    	src/main/java/telas/TelaMeusCursos.java
    	src/main/java/telas/TelaMinhasInfo.form
    	src/main/java/telas/TelaMinhasInfo.java
    	src/main/java/telas/TelaMinhasInfoAdm.form
    	src/main/java/telas/TelaMinhasInfoAdm.java
    	src/main/java/telas/TelaMostrarAlunos.form
    	src/main/java/telas/TelaMostrarAlunos.java
    	src/main/java/telas/Usuario.java

---
+++ TesteGit/src/main/java/telas/testegit/TesteGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package telas.testegit;
+
+/**
+ *
+ * @author andre
+ */
+public class TesteGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
